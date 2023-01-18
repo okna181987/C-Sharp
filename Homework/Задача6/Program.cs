75431a787f754a6f28002282a368ff3a536009d2
svburdiuk <okna181987@mail.ru>
2023-01-19 00:03:18 +0300

    Решил задачy 6

---
+++ Homework/Задача6/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите длину: ");
+int n = int.Parse(Console.ReadLine());
+    
+for (int i = 2; i <= n; i += 2)
+    Console.WriteLine(i);
