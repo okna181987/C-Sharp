11dda7743fc3f497657bade9570c0f1762128cd9
svburdiuk <okna181987@mail.ru>
2023-01-18 22:47:12 +0300

    Решил задачy 3

---
+++ Homework/Задача3/Program.cs
@@ -0,0 +1,14 @@
+﻿//По заданному номеру дня недели вывести его название 
+
+Console.WriteLine();
+string[] Days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+Console.Write("Введите номер дня недели:   ");
+string writeDay=Console.ReadLine();
+bool result = int.TryParse(writeDay, out int dayNumber);
+
+Console.WriteLine();
+if (result == false) Console.WriteLine("Число введено неверно");
+else if (0 < dayNumber && dayNumber < 8) Console.WriteLine($"{dayNumber}-й день недели - {Days[dayNumber - 1]}");
+else Console.WriteLine("Дня недели с таким номером не существует");
+Console.WriteLine();
