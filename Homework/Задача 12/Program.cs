94245601ba414b1f276329d02aaefb3a15e4515a
svburdiuk <okna181987@mail.ru>
2023-01-19 00:38:09 +0300

    Решил задачy 12

---
+++ Homework/Задача 12/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+System.Console.WriteLine();
+
+int Number=new Random().Next(1, 1000);
+System.Console.WriteLine();
+System.Console.WriteLine($"Дано число: {Number}");
+
+if (Number/100>0) System.Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number%10}");
+else System.Console.WriteLine($"У числа {Number} нет третьей цифры");
+
+System.Console.WriteLine();
