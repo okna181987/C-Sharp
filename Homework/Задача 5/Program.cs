7185afe411590d74616281e56ee467be74114838
svburdiuk <okna181987@mail.ru>
2023-01-18 23:40:16 +0300

    Решил задачy 5

---
+++ Homework/Задача 5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+int a;                             
+ Console.Write("введите число: ");
+ a= Console.Read();
+if (a % 2==1)
+    {
+     Console.WriteLine("число нечетное");
+    }
+    else
+    {
+    Console.WriteLine("Число четное");
+    }
+Console.ReadKey();
