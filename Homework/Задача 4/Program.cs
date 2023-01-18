02e844c1c83f339443539ddcf63bdfc07e9e5f31
svburdiuk <okna181987@mail.ru>
2023-01-18 22:52:30 +0300

    Решил задачy 4

---
+++ Homework/Задача 4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти максимальное из трех чисел
+int a = 10;
+int b = 2;
+int c = 60;
+
+int max =a ;
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
