d8f88521d84432e147371638a94a32bf0e4f0c9d
svburdiuk <okna181987@mail.ru>
2023-01-19 00:14:15 +0300

    Решил задачy 8

---
+++ Homework/Задача 8/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+System.Console.WriteLine();
+
+int number = new Random().Next(100, 1000);
+
+System.Console.WriteLine($"Вторая цифра числа {number} является цифра {number/10%10}");
+System.Console.WriteLine();
