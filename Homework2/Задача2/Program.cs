c3738cdce46c27af6855093ac2218dcd2b24aa2f
svburdiuk <okna181987@mail.ru>
2023-01-19 23:57:13 +0300

    задача 2

---
+++ Homework2/Задача2/Program.cs
@@ -0,0 +1,10 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Ведите число 1: ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ведите число 2: ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+
+int numberC = (int)Math.Sqrt(numberA);
+            if (numberB * numberB == numberA)
+                Console.WriteLine("yes");
+            else Console.WriteLine("no");
