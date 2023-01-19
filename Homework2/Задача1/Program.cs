14d5f00fd9ea06c1baf938a0c6b4bca1833b7b9d
svburdiuk <okna181987@mail.ru>
2023-01-19 23:47:23 +0300

    задача 1

---
+++ Homework2/Задача1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число, обозначающее день недели. Выяснить, является номер дня недели выходным
+
+Console.Clear();
+Console.WriteLine();
+
+int dayNumber = new Random().Next(1, 8);
+string[] Days = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+string txtResult = "";
+
+if (dayNumber > 5) txtResult = $". Ура! Это выходной день!";
+else txtResult = $", рабочий день.";
+
+Console.WriteLine();
+Console.WriteLine($"{dayNumber}-й день недели - это {Days[dayNumber - 1]}{txtResult}");
+Console.WriteLine();
