771318ad70716d4d5854454e45ad387cd356a4fd
svburdiuk <okna181987@mail.ru>
2023-01-25 22:51:20 +0300

    Решил задачу 7

---
+++ Homework4/Задача7/Program.cs
@@ -0,0 +1,26 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+Console.WriteLine();
+int size = new Random().Next(1, 99);
+Console.WriteLine($"Количетсов элементов массива: {size}");
+double[] FillSourceArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble() * 50;
+    }
+    return array;
+}
+void PrintDiffMinMax(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+    double difference = arr.Max() - arr.Min();
+    Console.WriteLine($"Минимальное значение массива {arr.Min()}, максимальное значение массива {arr.Max()}");
+    Console.WriteLine($"Разница между минимальным и максимальным значениями массива составляет {difference}");
+    Console.WriteLine();
+}
+PrintDiffMinMax(FillSourceArray(size));
+Console.WriteLine();
