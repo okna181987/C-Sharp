6f9a2fb4da0ddf32b74ab7f67a52b26f0d7a2e54
svburdiuk <okna181987@mail.ru>
2023-01-25 22:46:35 +0300

    Решил задачу 6

---
+++ Homework4/Задача6/Program.cs
@@ -0,0 +1,16 @@
+﻿//Найти произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+Console.WriteLine();
+int size = new Random().Next(5, 18);
+int min = 1;
+int max = 100;
+Console.WriteLine($"Количество элементов массива: {size}");
+int[] FillArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
