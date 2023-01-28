8f45eefc17ab3c746cc98c402dd5a05a1a2e9a89
svburdiuk <okna181987@mail.ru>
2023-01-28 12:55:01 +0300

    Закончил с ДЗ №3

---
+++ Homework5/Задача4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Написать программу копирования массива.
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+int[] CopyArray(int[] originArray)
+{
+    int[] copy = new int[originArray.Length];
+    for (int i = 0; i < originArray.Length; i++)
+    {
+        copy[i] = originArray[i];
+    }
+    return copy;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+int size = 10;
+int min = 1;
+int max = 100;
+int[] origin = GetArray(size, min, max);
+Console.WriteLine("Исходный массив А:");
+PrintArray(origin);
+Console.WriteLine();
+Console.WriteLine("Копия массива А:");
+int[] copy = CopyArray(origin);
