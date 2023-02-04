1c5b955f38d94080926db8a4b773656a96c9caf6
svburdiuk <okna181987@mail.ru>
2023-02-04 14:46:28 +0300

    Закончил с ДЗ №3

---
+++ Homework7/Задача3/Program.cs
@@ -0,0 +1,55 @@
+﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+int rowNumber(int[,] array)
+{
+    int index = 0;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (i == 0) result = sum;
+        else if (sum < result)
+        {
+            result = sum;
+            index = i;
+        }
+    }
+    return index;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+int row = 5;
+int col = 12;
+int min = 1;
+int max = 50;
+int[,] matrix = GetMatrix(row, col, min, max);
+PrintMatrix(matrix);
+Console.WriteLine("---");
+int index = rowNumber(matrix);
+Console.WriteLine(index);
