09837c68bc9f4d47449ce8a9b34021eaab7bbb16
svburdiuk <okna181987@mail.ru>
2023-02-08 14:26:55 +0300

    Закончил с ДЗ №3

--- Homework8/Задача4/Program.cs
+++ Homework8/Задача4/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿// Сформировать трехмерный массив не повторяющимися двузначными числами показать его
+// построчно на экран выводя индексы соответствующего элемента
+
+int[,,] array3D = new int[2, 2, 2];
+FillArray(array3D);
+PrintIndex(array3D);
+
+// Функция вывода индекса элементов 3D массива
+
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+// Функция заполнения 3D массива не повторяющимеся числами
+
+void FillArray(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
