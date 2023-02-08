3a9e5eb89d089c562a3e5a3da16bd8627c579995
svburdiuk <okna181987@mail.ru>
2023-02-08 14:08:49 +0300

    Закончил с ДЗ №3

--- Homework8/Задача5/Program.cs
+++ Homework8/Задача5/Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿//Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+
+int row = 99;
+int[,] triangle = new int[row, row];
+const int cellWidth = 1;
+void FillTriangle()
+{
+    for (int i = 0; i < row; i++)
+    {
+        triangle[i, 0] = 1;
+        triangle[i, i] = 1;
+    }
+    for (int i = 2; i < row; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            triangle[i, j] = triangle[i - 1, j - 1] + triangle[i - 1, j];
+        }
+    }
+}
+void PrintTriangle()
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 1; j < row; j++)
+        {
+            if (triangle[i, j] != 0)
+                Console.Write($"{triangle[i, j],cellWidth}");
+        }
+        Console.WriteLine();
+    }
+}
+void Magic()
+{
+    int col = cellWidth * row;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            Console.SetCursorPosition(col, i + 1);
+            if (triangle[i, j] % 2 != 0) Console.Write("*");
+            col += cellWidth * 2;
+        }
+        col = cellWidth * row - cellWidth * (i + 1);
+        Console.WriteLine();
+    }
+}
+Console.ReadLine();
+FillTriangle();
+Console.ReadLine();
+Magic();
