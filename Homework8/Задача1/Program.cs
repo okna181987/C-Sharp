f759d10a21ba27d8522159c01344bae04f0144fb
svburdiuk <okna181987@mail.ru>
2023-02-08 13:57:26 +0300

    Закончил с ДЗ №3

--- Homework8/Задача1/Program.cs
+++ Homework8/Задача1/Program.cs
@@ -1,1 +1,63 @@
-﻿// Составить частотный словарь элементов двумерного массива
+﻿//Составить частотный словарь элементов двумерного массива
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 6;
+int n = 6;
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int count = 0;
+int[,] dictionary = new int[m * n, 2];
+int k = 0;
+bool exist = false;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        exist = false;
+        for (int r = 0; r < k; r++)
+        {
+            if (dictionary[r, 0] == array[i, j])
+            {
+                dictionary[r, 1]++;
+                exist = true;
+                break;
+            }
+        }
+        if (exist == false)
+        {
+            dictionary[k, 0] = array[i, j];
+            dictionary[k, 1]++;
+            k++;
+        }
+    }
+}
+for (int i = 0; i < k; i++)
+{
+    for (int j = 0; j < dictionary.GetLength(1); j++)
+    {
+        Console.Write($"{dictionary[i, +j]} ");
+    }
+    Console.WriteLine();
+}
