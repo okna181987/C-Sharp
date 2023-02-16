f3a9a025a8e2158dfe2e7571d17f997d8db27933
svburdiuk <okna181987@mail.ru>
2023-02-16 23:59:47 +0300

    Закончил с ДЗ №3

---
+++ Homework9/Задача1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Показать натуральные числа от 1 до N, N задано
+
+// Console.WriteLine("Введите число N: ");
+// int N = int.Parse(Console.ReadLine());
+// string NumbersRec(int a, int N)
+// {
+//     if (a <= N) return $"{a} " + NumbersRec(a + 1, N);
+//     else return String.Empty;
+// }
+// Console.WriteLine(NumbersRec(1, N));
+
+void PrintNumber(int n)
+{
+    if (n < 1) return;
+    PrintNumber(n - 1);
+    Console.Write(n + " ");
+}
+PrintNumber(20);
